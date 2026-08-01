3a1d9b5b82dba4e348457baca59937c2f592c1e8
Akash <141831777+akashpatil05@users.noreply.github.com>
2026-08-01 21:24:50 +0530

    Time: 0 ms (100%), Space: 8.7 MB (98.58%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isValid(string s) {
+            stack<char> st;
+            for(char ch : s){
+                if(ch == '(' || ch =='[' || ch == '{'){
+                    st.push(ch);
+                }
+                else{
+                    if(st.size() == 0){
+                        return false;
+                    }
+                    else if(ch == ')' && st.top() != '('){
+                        return false;
+                    }
+                    else if(ch == '}' && st.top() != '{'){
+                        return false;
+                    }
+                    else if(ch == ']' && st.top() != '['){
+                        return false;
+                    }
+                    else{
+                        st.pop();
+                    }
+                }
+            }
+            return st.empty();
+    }
+};
