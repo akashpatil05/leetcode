9dc36384a2ad1ce771a6c9cb1040350c456d80c7
Akash <141831777+akashpatil05@users.noreply.github.com>
2026-09-06 23:21:10 +0530

    Time: 3 ms (66.2%), Space: 11 MB (15.84%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        unordered_map<char,int>need;
+        unordered_map<char,int>window;
+        for(char c : s1){
+            need[c]++;
+        }
+        int left = 0;
+        for(int right = 0; right< s2.length(); right++){
+            window[s2[right]]++;
+            if(right - left+ 1 > s1.length()){
+                window[s2[left]]--;
+                if(window[s2[left]] == 0){
+                    window.erase(s2[left]);
+                }
+                left++;
+            }
+            if(right-left+1 == s1.length()){
+                if(need == window){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
